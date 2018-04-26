fc569f7c8d1c65ab1141690b72a3178409e22484
RANHAOHR <rxh349@case.edu>
2018-04-26 13:33:05 -0500

    add seperate scheduler

--- scheduler.py
+++ scheduler.py
@@ -8,15 +8,17 @@ import pprint
 
 class Scheduler():
     """Implementation of the scheduler for synchronization"""
-    def __init__(self):
+    def __init__(self, sever_addr):
+        # self.server_addr = 
         self.context = zmq.Context()
         self.results_receiver = self.context.socket(zmq.PULL)
-        self.results_receiver.bind("tcp://127.0.0.1:5558")
+        self.results_receiver.bind("tcp://*:5559")
 
     def result_collector(self):
         collecter_data = {}
         for x in xrange(1000):
             result = self.results_receiver.recv_json()
+            print("result: ", result['consumer'])
             if collecter_data.has_key(result['consumer']):
                 collecter_data[result['consumer']] = collecter_data[result['consumer']] + 1
             else:
@@ -25,5 +27,6 @@ class Scheduler():
                 pprint.pprint(collecter_data)
 
 if __name__ == '__main__':
-    scheduler = Scheduler()
+    server_addr = sys.argv[1] if len(sys.argv) > 1 else "127.0.0.1"
+    scheduler = Scheduler(server_addr)
     scheduler.result_collector()
--- worker_node.py
+++ worker_node.py
@@ -6,18 +6,22 @@ import sys
 
 class Worker:
     """Implementation of the worker for gradient computation"""
-    def __init__(self, consumer_id, scheduler_addr):
+    def __init__(self, consumer_id, server_addr, scheduler_addr):
         self.consumer_id = consumer_id
         print "I am woker #%s" % (self.consumer_id)
         self.context = zmq.Context()
-        self.scheduler_addr = "tcp://" + scheduler_addr + ":5557"
-        self.worker_addr = "tcp://" + scheduler_addr + ":5558"
+        self.server_addr = "tcp://" + server_addr + ":5557"
+        self.worker_addr = "tcp://" + server_addr + ":5558"
+        self.scheduler_addr = "tcp://" + scheduler_addr + ":5559"
         # recieve work
         self.consumer_receiver = self.context.socket(zmq.PULL)
-        self.consumer_receiver.connect(self.scheduler_addr)
+        self.consumer_receiver.connect(self.server_addr)
         # send work
-        self.consumer_sender = self.context.socket(zmq.PUSH)
-        self.consumer_sender.connect(self.worker_addr)
+        self.server_sender = self.context.socket(zmq.PUSH)
+        self.server_sender.connect(self.worker_addr)
+
+        self.scheduler_sender = self.context.socket(zmq.PUSH)
+        self.scheduler_sender.connect(self.scheduler_addr)       
 
     def consumer(self):        
         while True:
@@ -26,13 +30,14 @@ class Worker:
             print("the received data is: ", data )
             result = { 'consumer' : self.consumer_id, 'num' : data}
             if data%2 == 0: 
-                self.consumer_sender.send_json(result)
+                self.server_sender.send_json(result)
                 print("sending to producer!")
+                self.scheduler_sender.send_json(result)
 
 if __name__ == '__main__':
     consumer_id = sys.argv[1] if len(sys.argv) > 1 else "10001"
-    scheduler_addr = sys.argv[2] if len(sys.argv) > 2 else "127.0.0.1"
-    port = sys.argv[3] if len(sys.argv) > 3 else ""
+    server_addr = sys.argv[2] if len(sys.argv) > 2 else "127.0.0.1"
+    scheduler_addr = sys.argv[3] if len(sys.argv) > 3 else "127.0.0.1"
 
-    worker = Worker(consumer_id, scheduler_addr)
+    worker = Worker(consumer_id, server_addr, scheduler_addr)
     worker.consumer()
